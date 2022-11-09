1a89e46d4bd41325bd218485e208ed744e13dc7f
user <valeriazhukovskaya789@gmail.com>
2022-11-09 15:33:32 +0200

    all projects FE

---
+++ project.7/script.js
@@ -0,0 +1,112 @@
+// alert("Hello World")
+// let name = prompt("Введите свое имя")
+// alert(name)
+// console.log(name) 
+
+// let name = prompt("Введите свое имя")
+// console.log("Hello, " + name)
+
+// let isRaining = true
+// if(isRaining == true)
+// alert("возьми зонт")
+
+
+// let isRaining = true 
+// if(isRaining == false) {
+// alert("возьми зонт")
+// } else {
+//     alert("Погода солнечная")
+// }
+
+// let degree = -2
+// if(degree > 15) {
+//     alert("виходь у футболці")
+// } else if(degree < 3) {
+//     alert("одягни куртку")
+// }
+// else {
+//     alert("одягни кофтину")
+// }
+
+// let sale =  
+// if(sale > 700) {
+//     alert("скидка 20%")
+// } else if (sale > 500) {
+//     alert("скидка 15%")
+// } else {
+//     alert("no sale")
+// }
+
+
+// let r = 125
+// let g = 50
+// let b = 200
+
+// let str = `rgb(${r}, ${g}, ${b})`
+// console.log(str)
+
+// let numb1 = +prompt("Введите первое число")
+// let numb2 = -prompt("Введите второе число")
+
+// if(numb1 > numb2) {
+//     console.log(numb1)
+// } 
+
+
+// let book = {
+//     name: "Garry Potter",
+//     how many books: "7",
+//     img: "https://img.jpg",
+//     gloves: ["",""]
+// }
+// console.log("book")
+// let book = ["Мастер и Маргарита", "Социальная драмма", "1937"]
+// console.log(book[0])
+
+// _____________________________
+// let nums1 = prompt("Введите первое число")
+// let nums2 = prompt("Введите второе число")
+// let nums3 = prompt("Введите третье число")
+
+// let nums = [100, 500]
+// nums.push(nums1, nums2, nums3) 
+// console.log(nums)
+
+// ___________
+// let nums = [10, 20, 30, 40, 50] 
+
+// console.log(nums[0])
+// console.log(nums[1])
+// console.log(nums[2])
+// console.log(nums[3])
+// console.log(nums[4])
+
+
+// _______ задача
+// let nums = [10, 20, 30, 40, 50] 
+
+// for(let i = 0; i < i] > 50; i++) {
+//      console.log(nums[i])
+// }
+
+let arr = [32, 51, 60, 78, 105, 14, 10]
+
+for(let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 50) {
+    console.log(arr[i])
+}
+}
+
+// 
+// for(let i = 1; i < 5; i++) {
+//     console.log(100)
+// }
+// ____________________
+// Задача
+// for(let i = 0; i <= 100; i++) {
+//  if( i > 30) {
+//     console.log("Больше", i)
+// } else {
+//     console.log(i)
+// }
+// }
